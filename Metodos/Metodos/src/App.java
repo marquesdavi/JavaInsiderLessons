51ff2f7d4193a4e6ceb367e94b9d2a7061c29495
Davi Marques <davibaterabatera@hotmail.com>
2022-11-26 15:50:51 -0300

    Adiciona parte final do exercício min array

--- Metodos/Metodos/src/App.java
+++ Metodos/Metodos/src/App.java
@@ -1,16 +1,18 @@
 public class App {
     public static void main(String[] args) {
         int[] array = { 3, 5, 9, 12, 52, -1, -129, 4 };
-        findMin(array);
+        int min = findMin(array);
+        System.out.println(min);
 
         int[] arrayTwo = new int[3];
         arrayTwo[0] = -8;
         arrayTwo[1] = 5;
         arrayTwo[2] = 12;
-        findMin(arrayTwo);
+        int minTwo = findMin(arrayTwo);
+        System.out.println(minTwo);
     }
 
-    static void findMin(int[] array) {
+    static int findMin(int[] array) {
         int min = Integer.MAX_VALUE;
 
         for (int e : array) {
@@ -18,7 +20,6 @@ public class App {
                 min = e;
             }
         }
-
-        System.out.println(min);
+        return min;
     }
 }
