ccc0c644853106ec827651a4e5036e56782657ef
marquesdavi <davibaterabatera@gmail.com>
2023-07-31 23:54:58 -0300

    Add new 0723 exercises

---
+++ JavaCore/KeyboardInput.java
@@ -0,0 +1,29 @@
+package JavaCore;
+
+public class KeyboardInput {
+    public static void main(String[] args) {
+        var scanner = new java.util.Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        String noteA = scanner.nextLine();
+
+        System.out.println("Digite a segunda nota: ");
+        String noteB = scanner.nextLine();
+
+        System.out.println("Digite a terceira nota: ");
+        String noteC = scanner.nextLine();
+
+        double result;
+        result = (
+                Double.parseDouble(noteA) +
+                Double.parseDouble(noteB) +
+                Double.parseDouble(noteC)) / 3;
+
+        if (result >= 7){
+            System.out.println("Você foi aprovado com média: " + result);
+        }
+        else{
+            System.out.println("Você foi reprovado com média: " + result);
+        }
+    }
+}
---
+++ JavaCore/StringOperations.java
@@ -0,0 +1,37 @@
+package JavaCore;
+
+public class StringOperations {
+    public static void main(String[] args){
+        // Criação de Strings
+
+        // Metodo 'new'
+        String viaMetodo = new String("Nova String");
+        System.out.println("Feita por método: " + viaMetodo);
+        // Declaração com aspas duplas
+        String strSimples = "Simples";
+        System.out.println("String de declaração simples: " + strSimples);
+
+        // Concatenação de Strings
+
+        // Concatenação simples
+        String nome = "Jubileu";
+        String sobrenome = "Da Silva";
+        String nomeCompleto = nome + " " + sobrenome;
+        System.out.println("Concatenação simples: " + nomeCompleto);
+
+        // Concatenação com o metodo .concat()
+        String viaConcat = nome + " ".concat(sobrenome);
+        System.out.println("Concatenação pelo metodo .concat(): " + viaConcat);
+
+        // Operações esteticas em strings
+
+        // Transforma a String em maiusculo
+        String tudoCaixaAlta = viaConcat.toUpperCase();
+        System.out.println("String em maiusculo: " + tudoCaixaAlta);
+
+        // Transforma a String em minusculo
+        String tudoCaixaBaixa = tudoCaixaAlta.toLowerCase();
+        System.out.println("String em minusculo: " + tudoCaixaBaixa);
+
+    }
+}
