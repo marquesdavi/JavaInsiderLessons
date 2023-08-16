84117793d55f552ccc371c20c80b6ae1d182b09b
marquesdavi <davibaterabatera@gmail.com>
2023-08-16 14:36:02 -0300

    class: switch case - Java Core

---
+++ JavaCore/SwitchCase.java
@@ -0,0 +1,24 @@
+package JavaCore;
+
+public class SwitchCase {
+
+    public static void main(String[] args) {
+        String escolheCar = "Corolla";
+        String carac = "A";
+
+        switch (escolheCar) {
+            case "Corolla":
+                carac = "Carro do tipo Sedan, rodas de aro 17, e fabricado pela Toyota.";
+                break;
+            case "Uno":
+                carac = "Carro do tipo Hert, rodas aro 13 e fabricado pela Fiat";
+                break;
+            default:
+                System.out.println("Nenhum");
+
+        }
+
+        System.out.println("Caracteristicas " + carac);
+    }
+
+}
